42b80b62ad29150d22077e126350c097c6e991b3
Preetha <preetha1999chennai@gmail.com>
2020-07-17 02:32:11 -0700

    Entire code has been changed by having two subclasses (Hierarchial inheritance)

--- shopkeeper_tax.py
+++ shopkeeper_tax.py
@@ -1,60 +1,68 @@
-class Shopkeeper:
-
+class Product:
     def __init__(self, id_str, name, price, category):
         self.id_str = id_str
         self.name = name
         self.price = price
         self.category = category
-        self.tax = 0
+        self.nrml_tax = 0
 
     def normal_tax(self):
         if self.price >= 500:
-            self.tax = 5
+            self.nrml_tax = self.price * (5 / 100)
         else:
-            self.tax = 2
-        return self.tax
+            self.nrml_tax = self.price * (2 / 100)
+        return self.nrml_tax
 
 
-class SpecialTax(Shopkeeper):
-
+class Textile(Product):
     def __init__(self, id_str, name, price, category):
-        self.spl_tax = 0
-        Shopkeeper.__init__(self, id_str, name, price, category)
+        super().__init__(id_str, name, price, category)
+        self.textl_tax = 0
 
-    def special_tax(self):
-        if self.category == "textile":
-            self.spl_tax = 1
-        elif self.category == "dairy" and self.price >= 1000:
-            self.spl_tax = 3
-        return self.spl_tax
+    def textile_tax(self):
+        self.textl_tax = Product.normal_tax(self) + self.price * (1 / 100)
+        return self.textl_tax
 
 
-def total_tax(*products):
-    tax_list = []
-    price_list_after_tax = []
-    for product in products:
-        final_tax = product.normal_tax() + product.special_tax()
-        tax_list.append(final_tax)
-        prod_price_after_tax = int(product.price * (1 + final_tax / 100))
-        price_list_after_tax.append(prod_price_after_tax)
-    print("Taxes for each products in percentage ", tax_list)
-    print("Prices of products after applying tax ", price_list_after_tax)
+class Dairy(Product):
+    def __init__(self, id_str, name, price, category):
+        super().__init__(id_str, name, price, category)
+        self.dair_tax = 0
+
+    def dairy_tax(self):
+        if self.price > 1000:
+            self.dair_tax = self.price * (3 / 100)
+        return round(self.dair_tax)
 
 
-prod1 = SpecialTax("09977", "apple", 250, "produce")
-prod2 = SpecialTax("6757687", "ghee", 400, "dairy")
-prod3 = SpecialTax("435657", "jeans", 1150, "textile")
-prod4 = SpecialTax("0825", "leggings", 300, "textile")
-prod5 = SpecialTax("0826", "leggings", 350, "textile")
-prod6 = SpecialTax("09800", "onion", 500, "produce")
-prod7 = SpecialTax("2409", "soap", 100, "home needs")
-prod8 = SpecialTax("8786", "utensil", 700, "home needs")
-prod9 = SpecialTax("455675", "ice-cream", 1001, "dairy")
-print("In the following list, taxes appear in the same order as their objects are instantiated")
-total_tax(prod1, prod2, prod3, prod4, prod5, prod6, prod7, prod8, prod9)
+prod1 = Product("09977", "apple", 250, "produce")
+prod2 = Dairy("6757687", "ghee", 400, "dairy")
+prod3 = Textile("435657", "jeans", 1150, "textile")
+prod4 = Textile("0825", "leggings", 300, "textile")
+prod5 = Textile("0826", "leggings", 350, "textile")
+prod6 = Product("09800", "onion", 500, "produce")
+prod7 = Product("2409", "soap", 100, "home needs")
+prod8 = Product("8786", "utensil", 700, "home needs")
+prod9 = Dairy("455675", "ice-cream", 1001, "dairy")
+print("Tax for {0} is: {1}".format(prod1.name, prod1.normal_tax()))
+print("Tax for {0} is: {1}".format(prod2.name, prod2.dairy_tax()))
+print("Tax for {0} is: {1}".format(prod3.name, prod3.textile_tax()))
+print("Tax for {0} is: {1}".format(prod4.name, prod4.textile_tax()))
+print("Tax for {0} is: {1}".format(prod5.name, prod5.textile_tax()))
+print("Tax for {0} is: {1}".format(prod6.name, prod6.normal_tax()))
+print("Tax for {0} is: {1}".format(prod7.name, prod7.normal_tax()))
+print("Tax for {0} is: {1}".format(prod8.name, prod8.normal_tax()))
+print("Tax for {0} is: {1}".format(prod9.name, prod9.dairy_tax()))
+
 '''
 Output:
-In the following list, taxes appear in the same order as their objects are instantiated
-Taxes for each products in percentage  [2, 2, 6, 3, 3, 5, 2, 5, 8]
-Prices of products after applying tax  [255, 408, 1219, 309, 360, 525, 102, 735, 1081]
+Tax for apple is: 5.0
+Tax for ghee is: 0
+Tax for jeans is: 69.0
+Tax for leggings is: 9.0
+Tax for leggings is: 10.5
+Tax for onion is: 25.0
+Tax for soap is: 2.0
+Tax for utensil is: 35.0
+Tax for ice-cream is: 30
 '''
