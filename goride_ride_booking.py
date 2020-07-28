a8af53dba2b080bad3e5f4c50a9d420f3ad2a64e
Preetha <preetha1999chennai@gmail.com>
2020-07-28 07:21:46 -0700

    Code to book rides in goride app

--- goride_ride_booking.py
+++ goride_ride_booking.py
@@ -17,38 +17,70 @@ class Ride:
     def get_ac_service(self):
         return self.__ac_service
 
-    def display_auto_price_menu(self):
-        print("\nCategory\tKm range\tPrice with AC\tPrice without AC\n")
-        print("Auto\t\tUpto 15\t\tRs.10/km\t\tNA")
-        print("Auto\t\t15 - 30\t\tRs.8/km\t\t\tNA")
-        print("Auto\t\t30 above\tRs.15/km\t\tNA")
 
-    def display_micro_price_menu(self):
-        print("\nCategory\tKm range\tPrice with AC\tPrice without AC\n")
-        print("Micro\t\tUpto 15\t\tRs.12/km\t\tRs.14/km")
-        print("Micro\t\t15 above\tRs.10/km\t\tRs.12/km")
+def display_auto_price_menu():
+    print("\nCategory\tKm range\tPrice with AC\tPrice without AC\n")
+    print("Auto\t\tUpto 15\t\tRs.10/km\t\tNA")
+    print("Auto\t\t15 - 30\t\tRs.8/km\t\t\tNA")
+    print("Auto\t\t30 above\tRs.15/km\t\tNA")
 
-    def display_xl_price_menu(self):
-        print("\nCategory\tKm range\tPrice with AC\tPrice without AC\n")
-        print("XL\t\t\tUpto 15\t\tRs.14/km\t\tRs.15/km")
-        print("XL\t\t\t15 above\tRs.14/km\t\tRs.15/km")
+
+def display_micro_price_menu():
+    print("\nCategory\tKm range\tPrice with AC\tPrice without AC\n")
+    print("Micro\t\tUpto 15\t\tRs.12/km\t\tRs.14/km")
+    print("Micro\t\t15 above\tRs.10/km\t\tRs.12/km")
+
+
+def display_xl_price_menu():
+    print("\nCategory\tKm range\tPrice with AC\tPrice without AC\n")
+    print("XL\t\t\tUpto 15\t\tRs.14/km\t\tRs.15/km")
+    print("XL\t\t\t15 above\tRs.14/km\t\tRs.15/km")
 
 
 def ride_booking():
     print("Vehicle categories:\nAuto - Can accommodate a maximum 3 people\nMicro - Can accommodate a maximum 4 people\nXL - Can accommodate a maximum 10 people")
-    vehicle_category_var = input("Enter the vehicle category you want to book: (example:auto, micro, xl)\n")
+    while True:
+        vehicle_category_var = input("Enter the vehicle category you want to book: (example:auto, micro, xl)\n")
+        if vehicle_category_var in ['auto', 'micro', 'xl']:
+            break
+        else:
+            print("You have entered a wrong category of vehicle")
+            continue
     if vehicle_category_var == "auto":
         ac_service_var = "no"
     else:
-        ac_service_var = input("If you want AC service enter 'yes' or else enter 'no'\nEnter your choice: ")
-    distance_var = int(input("Enter your ride distance in kilometers: (example: 20)\n"))
-    ride_object = Ride(vehicle_category_var, distance_var, ac_service_var)
+        while True:
+            ac_service_var = input("If you want AC service enter 'yes' or else enter 'no'\nEnter your choice: ")
+            if ac_service_var in ['yes', 'no']:
+                break
+            else:
+                print("You have entered wrong AC service choice")
+                continue
+    while True:
+        try:
+            distance_var = int(input("Enter your ride distance in kilometers: (example: 20)\n"))
+            break
+        except:
+            print("You have entered wrong distance")
+            continue
     if vehicle_category_var == "auto":
-        ride_object.display_auto_price_menu()
+        display_auto_price_menu()
     elif vehicle_category_var == "micro":
-        ride_object.display_micro_price_menu()
+        display_micro_price_menu()
     else:
-        ride_object.display_xl_price_menu()
+        display_xl_price_menu()
+    while True:
+        confirm_ride_booking = input("\nIf you want to book this ride enter 'yes' or else 'no'\nEnter your choice: ")
+        if confirm_ride_booking in ['yes', 'no']:
+            break
+        else:
+            print("You have entered wrong choice for confirmation")
+            continue
+    if confirm_ride_booking == 'yes':
+        ride_object = Ride(vehicle_category_var, distance_var, ac_service_var)
+        print("\nRide booked successfully !")
+    else:
+        pass
 
 
 ride_booking()
@@ -70,4 +102,8 @@ Category	Km range	Price with AC	Price without AC
 Micro		Upto 15		Rs.12/km		Rs.14/km
 Micro		15 above	Rs.10/km		Rs.12/km
 
+If you want to book this ride enter 'yes' or else 'no'
+Enter your choice: yes
+
+Ride booked successfully !
 '''
