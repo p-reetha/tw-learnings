912ed4811a8bd2f3ecb882638f6d14f415a253da
Preetha <preetha1999chennai@gmail.com>
2020-07-31 06:10:17 -0700

    Handled all the exceptions using regex

--- goride_car_registration.py
+++ goride_car_registration.py
@@ -1,4 +1,4 @@
-driver_objects_list = []
+import re
 
 
 class Car:
@@ -59,9 +59,24 @@ class Driver:
 
 def car_registration():
     driver_name_var = input("Enter the car driver/owner name: ")
-    driver_age_var = int(input("Enter driver age: "))
-    license_number_var = input("Enter driving license number: ")
-    license_validity_var = input("Enter driving license validity number: ")
+    while True:
+        driver_age_var = input("Enter driver age: ")
+        if re.match(r"[2-9][0-9]", driver_age_var):
+            break
+        else:
+            print("Enter age correctly")
+    while True:
+        license_number_var = input("Enter driving license number: ")
+        if re.match(r"([A-Z]{2}[0-9]{2}(19|20)[0-9][0-9])([0-9]{7})", license_number_var):
+            break
+        else:
+            print("Entered license number is not valid")
+    while True:
+        license_validity_var = input("Enter driving license validity number: ")
+        if re.match(r"(0?[1-9]|[12][0-9]|3[01])[-](0?[1-9]|1[012])[-]((19|20)[0-9]{2})$", license_validity_var):
+            break
+        else:
+            print("Entered license validity number is not valid")
     print("Car categories:\nMicro - All cars that can accommodate a maximum 4 people\nXL - All cars that can accommodate a maximum 10 people")
     while True:
         car_category_var = input("Enter your car category: (example:micro, xl)")
@@ -70,13 +85,17 @@ def car_registration():
         else:
             print("You have entered a wrong category of car")
             continue
-    car_number_var = input("Enter car number: ")
+    while True:
+        car_number_var = input("Enter car number: ")
+        if re.match(r"^[A-Z]{2}\s[0-9]{2}\s[A-Z]{2}\s[0-9]{4}$", car_number_var):
+            break
+        else:
+            print("Entered car number is not valid")
     car_colour_var = input("Enter car colour: ")
     car_company_var = input("Enter car company name: ")
     car_model_var = input("Enter car model name: ")
     car_object = Car(car_category_var, car_number_var, car_colour_var, car_company_var, car_model_var)
     driver_object = Driver(driver_name_var, driver_age_var, license_number_var, license_validity_var, car_object)
-    driver_objects_list.append(driver_object)
     print("\nRegistration successful !")
 
 
@@ -85,7 +104,7 @@ car_registration()
 Sample output:
 Enter the car driver/owner name: A.Preetha
 Enter driver age: 21
-Enter driving license number: MH14768768978776564
+Enter driving license number: MH1420110062821
 Enter driving license validity number: 04-12-2022
 Car categories:
 Micro - All cars that can accommodate a maximum 4 people
