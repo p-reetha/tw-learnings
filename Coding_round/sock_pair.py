e45d1062b93bf6a490891cc88077033dc2666288
Preetha <preetha1999chennai@gmail.com>
2020-08-31 17:36:57 +0530

    sock_string has been replaced with sorted_string

--- Coding_round/sock_pair.py
+++ Coding_round/sock_pair.py
@@ -5,11 +5,12 @@ if n % 2 == 0:
     sorted_string = sorted(sock_string)
     print(sorted_string)
     for i in range(0, n, 2):
-        if sock_string[i] == sorted_string[i+1]:
-            print("We have matches for all the socks")
+        if sorted_string[i] == sorted_string[i+1]:
+            continue
         else:
             print("Missing pair of socks")
             break
+    print("We have matches for all the socks")
 else:
     print("Missing pair of socks")
 
